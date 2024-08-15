a8f4b93a82cb9dd2bbe5d08f0299886e1f57b5d8
MMR <56675802+mmrabbani@users.noreply.github.com>
2024-08-16 00:35:50 +0330

    Optimise geo files

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
@@ -63,7 +63,7 @@ object AppConfig {
     const val v2rayNGIssues = "https://github.com/2dust/v2rayNG/issues"
     const val v2rayNGWikiMode = "https://github.com/2dust/v2rayNG/wiki/Mode"
     const val promotionUrl = "aHR0cHM6Ly85LjIzNDQ1Ni54eXovYWJjLmh0bWw="
-    const val geoUrl = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/";
+    const val geoUrl = "https://github.com/Chocolate4U/Iran-v2ray-rules/releases/latest/download/";
 
     const val DNS_AGENT = "1.1.1.1"
     const val DNS_DIRECT = "223.5.5.5"
